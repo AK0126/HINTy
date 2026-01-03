2cb8718226df687463871ab4cb894cced71f38b5
AK0126 <alex0126karbowski@gmail.com>
2026-01-02 21:30:41 -0500

    Add Error Handler

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import os
-
 import mlflow
 from dotenv import load_dotenv
+import traceback
 
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+
+# Force error output to stdout
+@app.errorhandler(500)
+def internal_error(error):
+    print("=" * 80, file=sys.stderr)
+    print("500 ERROR CAUGHT:", file=sys.stderr)
+    print(traceback.format_exc(), file=sys.stderr)
+    print("=" * 80, file=sys.stderr)
+    return jsonify({'error': 'Internal server error', 'details': str(error)}), 500
+
 
 # Set up MLflow
 def setup_mlflow():
